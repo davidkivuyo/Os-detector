2de9da6dc2fddb42c9e82ed8ef0ca69838513d08
David Larabal <davidkivuyo123@gmail.com>
2026-05-12 23:39:15 +0300

    [update] updated the OS feedback messages, also added accessibility label to button

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 let OS="UNKNOWN OS";
-if(navigator.appVersion.indexOf("Linux")!=-1){
+if(navigator.appVersion.indexOf("Linux")!==-1){
     document.getElementById('space').innerHTML='Linux';
-}else if(navigator.appVersion.indexOf("Win")!=-1){
+}else if(navigator.appVersion.indexOf("Win")!==-1){
     document.getElementById('space').innerHTML='Windows';
-}else if(navigator.appVersion.indexOf("Mac")!=-1){
-    document.getElementById('space').innerHTML='Mac';
-}else if(navigator.appVersion.indexOf("X11")!=-1){
-    document.getElementById('space').innerHTML='UNIX';
+}else if(navigator.appVersion.indexOf("Mac")!==-1){
+    document.getElementById('space').innerHTML='Macintosh';
+}else if(navigator.appVersion.indexOf("X11")!==-1){
+    document.getElementById('space').innerHTML='UNIX based';
 }else{
-    OS="whats your OS";
+    OS="whats your OS?";
     document.getElementById('space').innerHTML=OS;
 }

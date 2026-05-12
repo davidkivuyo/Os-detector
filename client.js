c2f5e815914514e35e4526c69361364e0c69777a
David Larabal <davidkivuyo123@gmail.com>
2026-05-13 00:00:02 +0300

    [update] added more information on user's agent platform details using userAgent

--- client.js
+++ client.js
@@ -1,13 +1,22 @@
-let OS="UNKNOWN OS";
+let OS;
 if(navigator.appVersion.indexOf("Linux")!==-1){
+    OS=navigator.userAgent;
     document.getElementById('space').innerHTML='Linux';
+    document.getElementById('info').innerHTML=OS;
 }else if(navigator.appVersion.indexOf("Win")!==-1){
+    OS=navigator.userAgent;
     document.getElementById('space').innerHTML='Windows';
+    document.getElementById('info').innerHTML=OS;
 }else if(navigator.appVersion.indexOf("Mac")!==-1){
+    OS=navigator.userAgent;
     document.getElementById('space').innerHTML='Macintosh';
+    document.getElementById('info').innerHTML=OS;
 }else if(navigator.appVersion.indexOf("X11")!==-1){
+    OS=navigator.userAgent;
     document.getElementById('space').innerHTML='UNIX based';
+    document.getElementById('info').innerHTML=OS;
 }else{
     OS="whats your OS?";
     document.getElementById('space').innerHTML=OS;
 }
+
